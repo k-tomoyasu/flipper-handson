1c2b6a13eadf9a0ec7c8475bec5de9fecadf689c
k-tomoyasu <blhicr1303@gmail.com>
2019-10-25 06:22:05 +0900

    remove unuse code

--- sample/android/app/src/main/java/com/facebook/flipper/sample/tutorial/TutorialApplication.kt
+++ sample/android/app/src/main/java/com/facebook/flipper/sample/tutorial/TutorialApplication.kt
@@ -8,44 +8,24 @@
 package com.facebook.flipper.sample.tutorial
 
 import android.app.Application
-import android.content.SharedPreferences
 import android.preference.PreferenceManager
 import com.facebook.drawee.backends.pipeline.Fresco
-import com.facebook.flipper.android.AndroidFlipperClient
-import com.facebook.flipper.plugins.fresco.FrescoFlipperPlugin
-import com.facebook.flipper.plugins.inspector.DescriptorMapping
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
-import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
 import com.facebook.litho.config.ComponentsConfiguration
-import com.facebook.soloader.SoLoader
 import okhttp3.OkHttpClient
-import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
-import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin
 
 class TutorialApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        SoLoader.init(this, false)
         Fresco.initialize(this)
 
         // Normally, you would want to make these dependent on BuildConfig.DEBUG.
         ComponentsConfiguration.isDebugModeEnabled = true
         ComponentsConfiguration.enableRenderInfoDebugging = true
 
-        val flipperClient = AndroidFlipperClient.getInstance(this)
-        val descriptorMapping = DescriptorMapping.withDefaults()
-        val networkFlipperPlugin = NetworkFlipperPlugin()
         Network.client = OkHttpClient.Builder()
-            .addInterceptor(FlipperOkhttpInterceptor(networkFlipperPlugin))
             .build()
         Preferences.pref = PreferenceManager.getDefaultSharedPreferences(applicationContext)
-
-        flipperClient.addPlugin(InspectorFlipperPlugin(this, descriptorMapping))
-        flipperClient.addPlugin(FrescoFlipperPlugin())
-        flipperClient.addPlugin(SharedPreferencesFlipperPlugin(applicationContext))
-        flipperClient.addPlugin(networkFlipperPlugin)
-        flipperClient.start()
     }
 }
 
